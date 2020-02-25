393854a4f84a17722e9b88129077e819fc03b0dc
esschillinger <52176799+esschillinger@users.noreply.github.com>
2020-02-25 10:10:20 -0600

    Finished last 3 scenes

--- master.py
+++ master.py
@@ -142,47 +142,47 @@ class SphericalCoordinates(ThreeDScene):
             lambda t : np.array([
                 0,
                 0,
-                2 * t
+                3 * t
             ]), t_min=0, t_max=1, color=BLUE
         )
 
         coordinates_r2 = ParametricFunction(
             lambda t : np.array([
-                (math.sqrt(3) * 1) * t,
+                (math.sqrt(3) * 3 / 2) * t,
                 0,
-                1 * t
+                3 / 2 * t
             ]), t_min=0, t_max=1, color=BLUE
         )
 
         coordinates_r = ParametricFunction(
             lambda t : np.array([
-                (2 * np.cos(PI/6) * np.sin(PI/3)) * t,
-                (2 * np.sin(PI/6) * np.sin(PI/3)) * t,
-                (2 * np.cos(PI/3)) * t
+                (3 * np.cos(PI/6) * np.sin(PI/3)) * t,
+                (3 * np.sin(PI/6) * np.sin(PI/3)) * t,
+                (3 * np.cos(PI/3)) * t
             ]), t_min=0, t_max=1, color=BLUE
         )
 
         coordinates_t = ParametricFunction(
             lambda t : np.array([
-                2 * np.cos(t),
-                2 * np.sin(t),
+                3 * np.cos(t),
+                3 * np.sin(t),
                 0
             ]), t_min=0, t_max=PI/6, color=WHITE
         )
 
         coordinates_s1 = ParametricFunction(
             lambda t : np.array([
-                (2 * np.sin(t)),
+                (3 * np.sin(t)),
                 0,
-                (2 * np.cos(t))
+                (3 * np.cos(t))
             ]), t_min=0, t_max=PI/3, color=RED
         )
 
         coordinates_s = ParametricFunction(
             lambda t : np.array([
-                (2 * np.cos(PI/6) * np.sin(t)),
-                (2 * np.sin(PI/6) * np.sin(t)),
-                (2 * np.cos(t))
+                (3 * np.cos(PI/6) * np.sin(t)),
+                (3 * np.sin(PI/6) * np.sin(t)),
+                (3 * np.cos(t))
             ]), t_min=0, t_max=PI/3, color=RED
         )
 
@@ -199,15 +199,56 @@ class SphericalCoordinates(ThreeDScene):
         text_r = TextMobject("r", color=BLUE)
         text_s = TextMobject("s", color=RED)
 
-        text_t.move_to(np.array([1/4, -2, 0]))
-        text_r.move_to(np.array([-1/2, -1/2, 0]))
-        text_s.move_to(np.array([2, 0, 0]))
+        text_t.move_to(np.array([1/4, -3/2, 0]))
+        text_r.move_to(np.array([-3/4, 2, 0]))
+        text_s.move_to(np.array([-3/4, 3, 0]))
 
         self.begin_ambient_camera_rotation()
         self.add_fixed_in_frame_mobjects(text_r)
-        self.play(ShowCreation(dashed_r1))
+        self.play(ShowCreation(dashed_r1), Write(text_r))
         self.wait()
-        self.play(ShowCreation(dashed_s1), ReplacementTransform(dashed_r1, dashed_r2))
+        self.play(ShowCreation(dashed_s1), ReplacementTransform(dashed_r1, dashed_r2), ReplacementTransform(text_r, text_s))
+        self.add_fixed_in_frame_mobjects(text_s)
         self.wait()
-        self.play(ShowCreation(dashed_t), ReplacementTransform(dashed_r2, dashed_r), ReplacementTransform(dashed_s1, dashed_s))
+        self.play(ShowCreation(dashed_t), ReplacementTransform(dashed_r2, dashed_r), ReplacementTransform(dashed_s1, dashed_s), ReplacementTransform(text_s, text_t))
+        self.add_fixed_in_frame_mobjects(text_t)
+        self.wait(2)
+
+
+class SphereTThenS(ThreeDScene):
+    def construct(self):
+        axes = ThreeDAxes()
+        self.set_camera_orientation(phi=75*DEGREES,theta=15*DEGREES)
+
+        self.add(axes)
+        self.begin_ambient_camera_rotation()
+
+        sphere = ParametricSurface(
+            lambda u, v: np.array([
+                3*np.cos(v)*np.sin(u),
+                3*np.sin(v)*np.sin(u),
+                3*np.cos(u)
+            ]), u_min=0, u_max=PI, v_min=0, v_max=TAU, checkerboard_colors=[RED_D, RED_E],
+            resolution=(15, 32))
+
+        self.play(LaggedStart(ShowCreation(sphere)), run_time=5)
+        self.wait(2)
+
+class SphereSThenT(ThreeDScene):
+    def construct(self):
+        axes = ThreeDAxes()
+        self.set_camera_orientation(phi=75*DEGREES,theta=-30*DEGREES)
+
+        self.add(axes)
+        self.begin_ambient_camera_rotation()
+
+        sphere = ParametricSurface(
+            lambda u, v: np.array([
+                3*np.cos(u)*np.sin(v),
+                3*np.sin(u)*np.sin(v),
+                3*np.cos(v)
+            ]), u_min=0, u_max=TAU, v_min=0, v_max=PI, checkerboard_colors=[RED_D, RED_E],
+            resolution=(15, 32))
+
+        self.play(LaggedStart(ShowCreation(sphere)), run_time=5)
         self.wait(2)
